04e56b7cce12a020c31454066cda3d6ec7718316
Shivam018-vk <shivam.yadav18361@sakec.ac.in>
2025-02-03 21:51:32 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".Text", {
+      strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      loop: true,
+    });
+  });
+  
